d6e5d24508ef4b7b4e2ec4666938a41b6b465cd5
mhassan88 <69553703+mhassan88@users.noreply.github.com>
2020-11-16 00:26:28 -0500

    Removed unused properties and functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { BadRequest } from './common/error-handling/bad-request';
-import { ProductsService } from './services/products.service';
 import { Component } from '@angular/core';
-import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +7,8 @@ import { CartService } from './services/cart.service';
 })
 export class AppComponent {
    title = 'Sowingo Front-end interview test - Muhammad Hassan';
-   products=[];
-   originalProductList=[];
   
-  constructor(productService : ProductsService, private cartService:CartService){
-    productService.getResource().subscribe(
-      result => {
-        this.products=result["hits"];
-        this.originalProductList=this.products;
-        this.cartService.addProducts(this.originalProductList);
-      },
-      error => {
-        alert("An unexpected error occured")
-      }
-    );
+  constructor(){
+
   }
 }
